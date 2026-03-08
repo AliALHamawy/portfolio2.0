ef02250200701a046e260bb725100090c8381f79
AliALHamawy <alialhamawy2007@gmail.com>
2026-03-08 08:31:18 -0700

    ending the logic in projects section incloding fillter

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -3,16 +3,34 @@ import './Projects.css'
 import { myProjects } from './myProjects'
 function Projects() {
     const [arr , setArr] = useState(myProjects);
+    const [currentActive , setCurrentActive] = useState("all")
+    const handleClick= (buttonCategory) => {
+        setCurrentActive(buttonCategory)
+        if(buttonCategory === "all") {
+            setArr(myProjects)
+            return;
+        }
+        const newArr = myProjects.filter(
+            (item) => item.category === buttonCategory
+        )
+        setArr(newArr)
+    }
     return (
         <>
             <div className="projects flex">
                 <div className="left-psection sticky w-55 grid grid-cols-1 ml-5 mr-2">
-                    <button>All Projects</button>
-                    <button>HTML & CSS</button>
-                    <button>JS</button>
-                    <button>BootStrape</button>
-                    <button>Tailwind</button>
-                    <button>React</button>
+                    <button
+                    onClick={() => {
+                        setCurrentActive("all")
+                        setArr(myProjects)
+                    }}
+                    className={currentActive=== "all" ? "active": null}>All Projects</button>
+                    <button
+                    onClick={() => handleClick("css")} className={currentActive === "css" ? "active" : null}>HTML & CSS</button>
+                    <button onClick={() => handleClick("js")} className={currentActive === "js" ? "active" : null}>JS</button>
+                    <button onClick={() => handleClick("bootstrap")} className={currentActive === "bootstrap" ? "active" : null}>BootStrap</button>
+                    <button onClick={() => handleClick("tailwind")} className={currentActive === "tailwind" ? "active" : null}>Tailwind</button>
+                    <button onClick={() => handleClick("react")} className={currentActive === "react" ? "active" : null}>React</button>
                 </div>
 
                 <div className="right-psection mx-auto">
