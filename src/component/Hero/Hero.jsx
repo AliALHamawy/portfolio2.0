51d2395f6b7653e7e83060908167b15bf82d6eb3
AliALHamawy <alialhamawy2007@gmail.com>
2026-03-16 11:14:49 -0700

    fix skills issu and add instagram link

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
                     <h3>Front-End Dev</h3>
                     <div className='icon-container'>
                         <a href="https://www.facebook.com/share/15y8VErk1w/" target='_blank' className='ri-facebook-box-line facebook '></a>
-                        <a href="http://localhost:5173/#" target='_blank' className='ri-instagram-line instagram'></a>
+                        <a href="https://www.instagram.com/alialhamawy?igsh=MTVjZTEzd29wNWtjeQ==" target='_blank' className='ri-instagram-line instagram'></a>
                         <a href="https://t.me/@alialhamawy" target='_blank' className='ri-telegram-line telegram'></a>
                         <a href="https://www.linkedin.com/in/ali-al-hamawy-1141b9393?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank' className='ri-linkedin-box-line linkedin'></a>
                         <a href="https://github.com/AliALHamawy" target='_blank' className='ri-github-line github'></a>
--- src/component/Skills/Skills.jsx
+++ src/component/Skills/Skills.jsx
@@ -2,16 +2,16 @@ import './Skills.css'
 
 function Skills() {
   return (
-    <div className='skills mt-10'>
+    <div className='skills '>
 
-      <div className=" flex flex-col md:flex-row justify-center mx-auto mt-10">
-        <div className='flex gap-10 md:mr-10 '>
+      <div className=" flex flex-col md:flex-row justify-center mx-auto mt-10 align-middle">
+        <div className=' axs flex gap-5 md:gap-10 md:mr-12'>
           <i className='ri-html5-line text-6xl gray icon html'></i>
           <i className='ri-css3-line text-6xl gray icon css'></i>
           <i className='ri-javascript-line text-6xl gray icon js'></i>
           <i className='ri-reactjs-line text-6xl gray icon reactjs'></i>
         </div>
-        <div className='flex gap-10'>
+        <div className='flex gap-5 md:gap-12'>
           <i className='ri-bootstrap-line text-6xl gray icon bootstrap'></i>
           <i className='ri-tailwind-css-line text-6xl gray icon tailwind-css'></i>
           <i className='ri-figma-line text-6xl gray icon figma'></i></div>
